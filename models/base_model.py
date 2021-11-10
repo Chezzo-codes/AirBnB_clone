2e22f886a6cb4bb09f535f6d1d7ab4dda1709b2d
PeterSimiyu <simiyupeter10@gmail.com>
2021-11-10 02:03:22 -0800

    dict representation

--- models/base_model.py
+++ models/base_model.py
@@ -2,8 +2,8 @@
 """
 Module contains BaseModel class
 """
-import uuid
-import datetime
+from uuid import uuid4
+from datetime import datetime
 import models
 
 
@@ -12,13 +12,21 @@ class BaseModel:
     class BaseModel that defines all
     common attributes/methods for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
-        Initialize instances
+        Initialize BaseModel attributes
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = self.created_at = datetime.today()
+        fmt = "%Y-%m-%dT%H:%M:S.%f"
+        if kwargs is not None:
+            for k, v in kwargs.items():
+                if k == "updated_at" or k == "created_at":
+                    self.__dict__[k] = datetime.strptime(v, fmt)
+                else:
+                    self.__dict__[k] = v
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -32,7 +40,13 @@ class BaseModel:
         """
         updates update_at - time
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
-        pass
+        """returns BaseModel dictionary instance"""
+        Bdict = self.dict__dict__.copy()
+        Bdict["created_at"] = self.created_at.isoformat()
+        Bdict["updated_at"] = self.updated_at.isoformat()
+        Bdict["__class__"] = self.__class__.__name__
+        return Bdict
