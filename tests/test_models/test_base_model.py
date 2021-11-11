e576464cf19acb568d580fd881bc31f967a16a11
Chezlin Drude <chezlindrude23@gmail.com>
2021-11-11 10:07:01 +0200

    Added missing init file for unittests and added more tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-Unittests for BaseModel class
+Unittest for BaseModel class
 """
-from models.base_model import BaseModel
 import unittest
+import os
 import pep8
-
+from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
 
@@ -17,14 +17,31 @@ class TestBaseModel(unittest.TestCase):
     def tearDownClass(cls):
         del cls.baseClass
 
-    def test_pep8_style(self):
+    def test_style_check(self):
         """
         Tests pep8 style
         """
         style = pep8.StyleGuide(quiet=True)
-        test = style.check_files(['models/base_model.py'])
-        self.assertEqual(test.total_errors, 0, "fix pep8")
+        p = style.check_files(['models/base_model.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
+    def test_check_functions(self):
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+        self.assertIsNotNone(BaseModel.to_dict.__doc__)
+
+    def test_init(self):
+        self.assertTrue(isinstance(self.baseClass, BaseModel))
+
+    def test_save(self):
+        self.baseClass.save()
+        self.assertNotEqual(self.baseClass.created_at, self.baseClass.updated_at)
+
+    def test_to_dict(self):
+        base12_dict = self.base12.to_dict()
+        self.assertEqual(self.base12.__class__.__name__, 'BaseModel')
+        self.assertIsInstance(base12_dict['created_at'], str)
+        self.assertIsInstance(base12_dict['updated_at'], str)
 
 if __name__ == "__main__":
     unittest.main()
